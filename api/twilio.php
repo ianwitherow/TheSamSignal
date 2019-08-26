0fa7c9d00d72fb1ef58f25816f674c4b071c28ff
Ian Witherow <ianwitherow@gmail.com>
2019-08-26 17:26:01 -0600

    hide error messages

--- api/twilio.php
+++ api/twilio.php
@@ -1,8 +1,8 @@
 <?php
 
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
+//ini_set('display_errors',1);
+//ini_set('display_startup_errors',1);
+//error_reporting(-1);
 
 require "vendor/twilio/sdk/Services/Twilio.php";
 $configs = include('../config.php');
@@ -16,10 +16,10 @@ $AccountSid = $configs['AccountSid'];
 $AuthToken = $configs['AuthToken'];
 
 if (isset($_GET["textSam"]) && $_GET["textSam"] === "1") {
-	//Only allow this to run every 5 minutes
-	if (isset($_SESSION["lastSent"]) == false || minutesBetween($_SESSION["lastSent"]) >= 5) {
-		//Check PIN
-		if (isset($_GET["pin"]) && $_GET["pin"] === $SamPin) {
+	// Check PIN
+	if (isset($_GET["pin"]) && $_GET["pin"] === $SamPin) {
+		// Only allow this to run every 5 minutes
+		if (isset($_SESSION["lastSent"]) == false || minutesBetween($_SESSION["lastSent"]) >= 5) {
 
 
 
@@ -35,20 +35,27 @@ if (isset($_GET["textSam"]) && $_GET["textSam"] === "1") {
 			$_SESSION["lastSent"] = new DateTime();
 			return;
 
-		}
-		else if (isset($_GET["pin"]) && $_GET["pin"] === "test") {
-			echo "true";
-			return;
-		} else { //PIN check
-			echo "false";
-			return;
-		} 
-
 		} else {
+			// Trying to send too often
 			echo "false";
 			return;
 		}
 
-	}
+	} else if (isset($_GET["pin"]) && $_GET["pin"] === "test") {
+		echo "true";
+		return;
+	} else {
+		// PIN check failed
+		echo "false";
+		return;
+	} 
+
+
+}
+
+function minutesBetween($date1) {
+	$since_date = $date1->diff(new DateTime());
+	return $since_date->i;
+}
 
 ?>
